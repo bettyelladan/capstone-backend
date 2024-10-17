575e14bc9757c321b96ba5bccabe59fbc8224287
bettyelladan <danieba@mail.uc.edu>
2024-10-17 14:30:20 -0400

    backend

---
+++ models/Product.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: String,
+    category: String,
+    stock: {
+        type: Number,
+        default: 0,
+    },
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
